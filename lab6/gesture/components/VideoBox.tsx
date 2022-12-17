f00a239b0feaa21ddaf1130254909edc03f25e88
Jakub Jablonski <jjablonski.it@gmail.com>
2022-12-17 21:57:05 +0100

    add recognition

--- lab6/gesture/components/VideoBox.tsx
+++ lab6/gesture/components/VideoBox.tsx
@@ -1,11 +1,13 @@
-import { HandPose, load as loadHandpose } from '@tensorflow-models/handpose'
+import { AnnotatedPrediction, HandPose, load as loadHandpose } from '@tensorflow-models/handpose'
 import '@tensorflow/tfjs'
-import { useCallback } from 'react'
+import { useCallback, useState } from 'react'
+import {} from 'fingerpose'
 
 const HEIGHT = 500
 const WIDTH = 500
 
 export const VideoBox = () => {
+  const [emoji, setEmoji] = useState('🤷‍♀️')
   const handleVideoRef = useCallback(async (videoRef: HTMLVideoElement) => {
     const stream = await navigator.mediaDevices.getUserMedia({
       video: true,
@@ -22,15 +24,48 @@ export const VideoBox = () => {
     }, 100)
   }
 
-  const checkForHand = (handpose: HandPose, videoRef: HTMLVideoElement) => {
+  const checkForHand = async (handpose: HandPose, videoRef: HTMLVideoElement) => {
     if (!videoRef) return
 
-    handpose.estimateHands(videoRef).then((predictions) => {
-      if (predictions.length > 0) {
-        console.log('Hand detected')
-      }
-    })
+    const prediction = await handpose.estimateHands(videoRef)
+    if (prediction.length > 0) {
+      const emoji = mapGestureToEmoji(prediction[0])
+      emoji && setEmoji(emoji)
+    }
   }
 
-  return <video ref={handleVideoRef} height={HEIGHT} width={WIDTH} autoPlay />
+  return (
+    <>
+      <video ref={handleVideoRef} height={HEIGHT} width={WIDTH} autoPlay />
+      <h1>{emoji}</h1>
+    </>
+  )
+}
+
+const mapGestureToEmoji = (prediction: AnnotatedPrediction) => {
+  if (!prediction) {
+    return '🤷‍♀️'
+  }
+
+  // Find the most confident hand gesture
+  const mostConfidentIndex = prediction.landmarks.reduce((iMax, x, i, arr) => (x[0] > arr[iMax][0] ? i : iMax), 0)
+  console.log(mostConfidentIndex)
+
+  // Map the hand gesture to an emoji
+  switch (mostConfidentIndex) {
+    case 4:
+    case 10:
+    case 7:
+    case 6:
+      return '👍'
+    case 8:
+      return '✌️'
+    // case 18:
+    //   return '🖕'
+    // case 12:
+    //   return '👌'
+
+    default:
+      return null
+  }
 }
